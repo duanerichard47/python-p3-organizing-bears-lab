f8df953778a8fd69e28921fcfeb73e478673ef83
Duane <duanerichard47@hotmail.com>
2023-02-26 13:08:01 -0500

    complete

---
+++ lib/create.sql
@@ -0,0 +1,2 @@
+CREATE Table bears (
+id INTEGER PRIMARY KEY, name TEXT, age INTEGER, sex TEXT, color TEXT, temperament TEXT, alive BOOLEAN);
---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name,age,sex,color,temperament) VALUES ('Mr. Chocolate', 30, 'M', 'brown', 'gentle');
+INSERT INTO bears (name,age,sex,color,temperament) VALUES ('Rowdy', 15, 'M', 'grizzly', 'agressive');
+INSERT INTO bears (name,age,sex,color,temperament) VALUES ('Tabitha', 20, 'F', 'black', 'gentle');
+INSERT INTO bears (name,age,sex,color,temperament) VALUES ('Sergeant Brown', 30, 'M', 'grizzly', 'gentle');
+INSERT INTO bears (name,age,sex,color,temperament) VALUES ('Melissa', 8, 'F', 'brown', 'aggressive');
+INSERT INTO bears (name,age,sex,color,temperament) VALUES ('Grinch', 15, 'M', 'polar', 'moderate');
+INSERT INTO bears (name,age,sex,color,temperament) VALUES ('Wendy', 25, 'F', 'panda', 'gentle');
+INSERT INTO bears (name,age,sex,color,temperament) VALUES (NULL, 20, 'M', 'kodiak', 'gentle');
+
+
+
